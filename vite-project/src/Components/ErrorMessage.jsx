565749ad8295dd579f4e70649738c97f529300fd
Andrew De Guzman <drewndrei@gmail.com>
2022-09-23 15:25:27 +0800

    Add ErrorMessage Component

---
+++ vite-project/src/Components/ErrorMessage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ErrorMessage() {
+  return (
+    <div className='game--check-button absolute'>Answer all questions first</div>
+  )
+}
+
+export default ErrorMessage;
